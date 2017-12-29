a7981372e978659306445ad54d788ec59f84b308
Diego <diego.camara@pitang.com>
2017-12-29 15:12:36 -0300

    Inclusão do WebExceptionHandler

--- simplemvc/src/main/java/com/example/simplemvc/controller/SimpleMVCController.java
+++ simplemvc/src/main/java/com/example/simplemvc/controller/SimpleMVCController.java
@@ -6,16 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.simplemvc.mediator.ICRUDMediator;
 import com.example.simplemvc.mediator.ISimpleMVCMediator;
 import com.example.simplemvc.model.Simple;
-import com.example.simplemvc.provider.SpringMessageProvider;
 
 @RestController
 public class SimpleMVCController extends AbstractController<Simple, Integer> implements ISimpleMVCController {
 
 	@Autowired
 	private ISimpleMVCMediator simpleMVCMediator;
-
-	@Autowired
-	private SpringMessageProvider springMessageProvider;
 
 	@Override
 	public ICRUDMediator<Simple, Integer> getCRUDMediator() {
--- simplemvc/src/main/java/com/example/simplemvc/provider/ConstantsMessages.java
+++ simplemvc/src/main/java/com/example/simplemvc/provider/ConstantsMessages.java
@@ -2,4 +2,6 @@ package com.example.simplemvc.provider;
 
 public class ConstantsMessages {
 
+	public static final String UNEXPECTED_ERROR = "unexpected.error";
+
 }

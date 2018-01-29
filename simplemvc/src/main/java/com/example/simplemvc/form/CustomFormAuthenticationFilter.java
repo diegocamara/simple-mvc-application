de807251b7a796a90e7f178e6625f0a676c76af4
Diego <diego.camara@pitang.com>
2018-01-29 18:34:27 -0300

    Testes com FormAuthenticationProvider

---
+++ simplemvc/src/main/java/com/example/simplemvc/form/CustomFormAuthenticationFilter.java
@@ -0,0 +1,118 @@
+package com.example.simplemvc.form;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import com.example.simplemvc.jwt.JwtUtil;
+import com.example.simplemvc.jwt.TokenTO;
+import com.example.simplemvc.mediator.UserMediator;
+
+public class CustomFormAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+	private static final String CHARACTER_ENCODING_UTF_8 = "UTF-8";
+	private String jwtSecretKey;
+
+	private UserMediator userDetailsService;
+
+	public CustomFormAuthenticationFilter(RequestMatcher requiresAuthenticationRequestMatcher,
+			UserMediator userDetailsService, String secretKey) {
+		super(requiresAuthenticationRequestMatcher);
+		this.userDetailsService = userDetailsService;
+		this.jwtSecretKey = secretKey;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		// TODO validate username password
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+		return this.getAuthenticationManager().authenticate(token);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authentication) throws IOException, ServletException {
+		response.setStatus(HttpStatus.OK.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding(CHARACTER_ENCODING_UTF_8);
+
+		JSONObject jsonResponse = new JSONObject();
+		String username = (String) authentication.getPrincipal();
+		String jwtToken = JwtUtil.getToken(jwtSecretKey,
+				buildTokenTO(userDetailsService.loadUserByUsername(username), request));
+		try {
+			jsonResponse.put(JwtUtil.JWT_TOKEN, jwtToken);
+			jsonResponse.put("message", "Login Successful");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		response.getWriter().write(jsonResponse.toString());
+		response.setHeader(JwtUtil.HEADER_APP_SUBJECT, authentication.getName());
+	}
+
+	private TokenTO buildTokenTO(UserDetails user, HttpServletRequest request) {
+		LocalDateTime ldt = LocalDateTime.now().plusMinutes(15);
+		Date expirationDate = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+		Map<String, Object> headerClaims = new HashMap<>();
+		headerClaims.put("type", "JWT");
+
+		TokenTO tokenTO = new TokenTO();
+		tokenTO.setSubjectId(user.getUsername());
+		tokenTO.setSubject(user.getUsername());
+		tokenTO.setClientIpAddress(request.getRemoteAddr());
+		tokenTO.setBrowserFingerprintDigest(request.getHeader(JwtUtil.USER_AGENT));
+		tokenTO.setIssueDate(new Date());
+		tokenTO.setIssuer(JwtUtil.AUTH_SERVICE_JWT);
+		tokenTO.setExpirationDate(expirationDate);
+		tokenTO.setRoles(getUserRoles(user));
+		tokenTO.setHeaderClaims(headerClaims);
+
+		return tokenTO;
+	}
+
+	private String getUserRoles(UserDetails user) {
+		return String.join(",", user.getAuthorities().stream().map(r -> r.getAuthority()).collect(Collectors.toList()));
+	}
+
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+		response.setStatus(HttpStatus.BAD_REQUEST.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding(CHARACTER_ENCODING_UTF_8);
+		JSONObject jsonResponse = new JSONObject();
+		try {
+			jsonResponse.put("message", "Invalid Credentials");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		response.getWriter().write(jsonResponse.toString());
+	}
+
+}
--- simplemvc/src/main/java/com/example/simplemvc/model/User.java
+++ simplemvc/src/main/java/com/example/simplemvc/model/User.java
@@ -72,4 +72,13 @@ public class User implements UserDetails {
 	public String getUsername() {
 		return username;
 	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }

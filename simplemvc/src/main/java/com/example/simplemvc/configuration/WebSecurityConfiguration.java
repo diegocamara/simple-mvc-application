b168ff1ae95a9cf0a9233318589cc0051c3f8343
Diego <diego.camara@pitang.com>
2018-02-05 10:50:26 -0300

    Testes com cors filter

--- simplemvc/src/main/java/com/example/simplemvc/configuration/WebSecurityConfiguration.java
+++ simplemvc/src/main/java/com/example/simplemvc/configuration/WebSecurityConfiguration.java
@@ -9,14 +9,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 import com.example.simplemvc.form.FormAuthenticationProvider;
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -39,8 +37,26 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		// http.addFilterBefore(corsFilter(),
+		// UsernamePasswordAuthenticationFilter.class);
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().httpBasic()
 				.realmName("realmName").and().csrf().disable();
 	}
+
+	// @Bean
+	// public CorsFilter corsFilter() {
+	// CorsConfiguration configuration = new CorsConfiguration();
+	// configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+	// configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
+	// "DELETE", "OPTIONS"));
+	// configuration.setAllowedHeaders(Arrays.asList("authorization",
+	// "content-type", "x-auth-token"));
+	// configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+	// UrlBasedCorsConfigurationSource source = new
+	// UrlBasedCorsConfigurationSource();
+	// source.registerCorsConfiguration("/**", configuration);
+	// return new CorsFilter(source);
+	//
+	// }
 
 }
